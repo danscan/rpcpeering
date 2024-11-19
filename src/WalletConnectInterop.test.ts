8daaaf1078432d747d630efd327dab83efc4f9a8
danscan <dan@dscanlon.com>
2024-11-19 14:22:29 -0500

    feat: implement WalletConnectInterop

---
+++ src/WalletConnectInterop.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'bun:test';
+import { upgradeWCUri, parseUpgradeWCUri, base64UrlEncode, uriIncludesSessionProposal } from './WalletConnectInterop';
+
+const PROPOSAL = { topic: 'test', url: 'https://example.rpcpeering.org', advertise: ['test2'] };
+
+const WC_URI = 'wc:1f12611ce3fb892fc4ecf3ca842e6d33d59a91c96dce38ab75b4b0e4a1a7de84@2?expiryTimestamp=1732043830&relay-protocol=irn&symKey=1e51dca72ece64c8725fe5372d410f32459156be233c05f52063cfad3971067c';
+const WC_URI_WITH_PROPOSAL = `${WC_URI}&org.rpcpeering.proposal=${encodeURIComponent(base64UrlEncode(JSON.stringify(PROPOSAL)))}`;
+
+describe('uriIncludesSessionProposal', () => {
+  it('should return true if the WC URI includes a peering proposal', () => {
+    expect(uriIncludesSessionProposal(WC_URI_WITH_PROPOSAL)).toBe(true);
+  });
+
+  it('should return false if the WC URI does not include a peering proposal', () => {
+    expect(uriIncludesSessionProposal(WC_URI)).toBe(false);
+  });
+});
+
+describe('upgradeWCUri', () => {
+  it('should upgrade a WC URI to a peering URI', () => {
+    const upgraded = upgradeWCUri(WC_URI, PROPOSAL);
+    expect(upgraded).toEqual(WC_URI_WITH_PROPOSAL);
+  });
+});
+
+describe('parseUpgradeWCUri', () => {
+  it('should parse a peering URI from a WC URI', () => {
+    const parsed = parseUpgradeWCUri(WC_URI_WITH_PROPOSAL);
+    expect(parsed).toEqual(PROPOSAL);
+  });
+});
